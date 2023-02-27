9f4110da6a4967a56d5a3440112b9890149b49be
kok-s0s <kok_s0s@163.com>
2023-02-27 13:38:20 +0800

    style: code format

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,4 +1,4 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss, { pagesGlobToRssItems } from '@astrojs/rss'
 
 export async function get() {
   return rss({
@@ -7,5 +7,5 @@ export async function get() {
     site: 'https://my-blog-site.netlify.app',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
-  });
+  })
 }
