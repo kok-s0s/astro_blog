80cd8add09c91b381f2f37c1edf235cfca4f655d
kok-s0s <kok_s0s@163.com>
2023-04-14 16:37:44 +0800

    feat: update copy function and its style

--- src/scripts/copy.js
+++ src/scripts/copy.js
@@ -1,25 +1,26 @@
 ;[...document.getElementsByTagName('pre')].forEach((item) => {
-  item.style.position = 'relative'
   let copyButton = document.createElement('button')
   copyButton.style.cssText =
-    'position: relative; right: 1rem; z-index: 555 !important; float: right; border: none; border-radius: 8px; padding: 6px 8px; margin-bottom: -1rem; font-size: 1.1rem; font-weight: bold; letter-spacing: 1px; cursor: pointer; background-color: #002b36; color: #808080'
-  if (window.screen.width < 636) {
-    copyButton.style.cssText = 'display: none;'
-    item.style.marginTop = '0.5rem'
-  } else {
-    item.style.marginTop = '0'
-  }
+    'position: relative; right: -0.5rem; top: -0.5rem; z-index: 555 !important; float: right; border-color: rgba(205,217,229,0.1); padding: 0; border-radius: 6px; cursor: pointer; background-color: #2e3440;'
 
-  copyButton.innerHTML = 'Copy'
+  copyButton.innerHTML = `<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-copy js-clipboard-copy-icon m-2">
+                            <path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"></path>
+                            <path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"></path>
+                          </svg>`
+
   copyButton.onclick = function () {
-    let copyData = item.firstChild.innerText
+    let copyData = item.lastChild.innerText
     copyToClipboard(copyData)
-    copyButton.innerHTML = 'Done'
+    copyButton.innerHTML = `<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-check js-clipboard-check-icon color-fg-success m-2 d-none">
+                              <path d="M13.78 4.22a.75.75 0 0 1 0 1.06l-7.25 7.25a.75.75 0 0 1-1.06 0L2.22 9.28a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018L6 10.94l6.72-6.72a.75.75 0 0 1 1.06 0Z"></path>
+                            </svg>`
     setTimeout(function () {
-      copyButton.innerHTML = 'Copy'
+      copyButton.innerHTML = `<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-copy js-clipboard-copy-icon m-2">
+            <path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"></path><path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"></path>
+        </svg>`
     }, 1000)
   }
-  item.insertAdjacentElement('beforebegin', copyButton)
+  item.insertAdjacentElement('afterbegin', copyButton)
 })
 
 function copyToClipboard(content) {
@@ -33,6 +34,6 @@ function copyToClipboard(content) {
         document.oncopy = null
       }
     })(content)
-    document.execCommand('Copy')
+    document.execCommand('copy')
   }
 }
