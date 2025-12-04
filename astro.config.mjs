f25ef8cd4f4cc692c2f9e1f63fd583fd59fb33b3
kok_s0s <kok_s0s@163.com>
2025-12-04 17:44:52 +0800

    feat: add math post with KaTeX support and update configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 import preact from '@astrojs/preact'
 
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
   markdown: {
     // Can be 'shiki' (default), 'prism' or false to disable highlighting
     syntaxHighlight: 'prism',
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [[rehypeKatex, { strict: false }]],
   },
 })
